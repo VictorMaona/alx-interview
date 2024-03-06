5771732455116d16b6cdd0132a5cdfa574c156ef
VictorMaona <maonavictor@gmail.com>
2024-03-06 11:05:02 +0000

    updated

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Pascal Triangle Interview Question"""
+
+
+def pascal_triangle(n):
+    """list of lists of numbers is returned
+    illustrating the Pascal Triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # specify row and insert 1 into first and last idx
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
