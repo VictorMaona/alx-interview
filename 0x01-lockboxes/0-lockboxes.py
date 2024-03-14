274f49cb7d1503ec3caf605d65e68b028a0050b3
VictorMaona <maonavictor@gmail.com>
2024-03-14 15:31:44 +0000

    updated

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Task for lists representing the boxes'''
+
+
+def canUnlockAll(boxes):
+    '''accepts a single argument box
+    Returns:
+        True: Every box can be opened.
+        False: Not every package can be opened.
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
+
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,9 @@
+#0x01. Lockboxes
+
+With the keys at hand, this research seeks to put into practice a way to ascertain whether 
+or not all of the boxes can be opened. The objective is to find out if it's possible to 
+open every box beginning with the first one, which is already unlocked, as each box can 
+hold keys to other boxes.
+
+Files main 0.py: Python script to test the canUnlockAll function in various scenarios; 
+Files 0 lockboxes.py: Python file contains the implementation of the canUnlockAll method.
